3dab641c8019491ca6c8e181d81750d8b8621798
Chris <47066922+cwilso28@users.noreply.github.com>
2024-12-31 16:55:45 -0600

    Loop to generate multiple etch-a-sketch cells
    Create a loop to generate cells for the etch-a-sketch so that each are
    accessible from the css stylesheet to apply styles to the cells
    simultaneously

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// INIT a function to generate each block for the etch-a-sketch
+    // INIT a variable with the total number of blocks in the etch-a-sketch by squaring the blockPerSide
+    // INIT a loop that loops the total number of blocks
+        // Create a div block with the class "cell"
+        // Append the cell block to the container
+
+function createBlocks(blocksPerSide) {
+    let totalBlocks = blocksPerSide ** 2;
+    let containerDiv = document.querySelector("#container")
+    for (i = 0; i < totalBlocks; i++) {
+        cell = document.createElement("div");
+        cell.classList.add("cell");
+        containerDiv.appendChild(cell);
+    }
+}
