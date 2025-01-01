419daff99849de5115996c91289e5cd0db0f04cb
Chris <47066922+cwilso28@users.noreply.github.com>
2025-01-01 14:38:40 -0600

    Change opacity as the mouse re-enters cell
    The opacity increases in each cell that the mouse passes over

--- javascript.js
+++ javascript.js
@@ -2,10 +2,13 @@
 function createBlocks(blocksPerSide) {
     let totalBlocks = blocksPerSide ** 2;
     let containerDiv = document.querySelector("#container");
+    let flexBasisSetting = 100 / blocksPerSide;
     for (i = 0; i < totalBlocks; i++) {
         cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.style.backgroundColor = "rgb(255 0 255 / 1.0)"
+        cell.style.flexBasis = flexBasisSetting + "%";
+        cell.style.backgroundColor = "rgb(255 0 255)"
+        cell.style.opacity = "0.1"
         containerDiv.appendChild(cell);
     };
 };
@@ -13,9 +16,13 @@ function createBlocks(blocksPerSide) {
 function resetGrid() {
     const cellList = document.querySelectorAll(".cell");
     for (let i = 0; i < cellList.length; i++) {
-        cellList[i].style.backgroundColor = "rgb(255 255 0 / 1.0)"
+        cellList[i].style.backgroundColor = "rgb(255 255 0)"
     };
 };
+
+function parseRGBA(rgbaValues) {
+
+}
 
 createBlocks(4);
 
@@ -26,9 +33,12 @@ let containerDiv = document.querySelector("#container");
 containerDiv.addEventListener("mouseover", function (e) {
     if (e.target && e.target.matches(".cell")) {
         const enteredCell = e.target;
+        cellBackground = enteredCell.style.opacity;
+        console.log(cellBackground);
         // cellOpacity = document.getComputedStyle(enteredCell).getPropertyValue("opacity");
         // cellOpacity += 0.1;
-        enteredCell.style.backgroundColor = "rgb(0 0 0 / 1.0)"
+        enteredCell.style.backgroundColor = "rgb(0 0 0)"
+        enteredCell.style.opacity = String(Number(cellBackground) + 0.1);
         // enteredCell.style.backgroundColor = "black";
         // enteredCell.style.opacity = cellOpacity;
     };
