dec421898febd1b335d9957cbbb3b6b1299b3f9b
Chris <47066922+cwilso28@users.noreply.github.com>
2025-01-01 11:49:07 -0600

    Button resets the grid when clicked
    There needs to be a way to reset the grid without refreshing the page

--- javascript.js
+++ javascript.js
@@ -5,16 +5,24 @@ function createBlocks(blocksPerSide) {
     for (i = 0; i < totalBlocks; i++) {
         cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.setAttribute("style", "background: white;");
         containerDiv.appendChild(cell);
-    }
-}
+    };
+};
+
+function resetGrid() {
+    const cellList = document.querySelectorAll(".cell");
+    for (let i = 0; i < cellList.length; i++) {
+        cellList[i].setAttribute("style", "background: white;");
+    };
+};
 
 createBlocks(4);
 
 let cellDiv = document.querySelector(".cell");
 let containerDiv = document.querySelector("#container");
 
-containerDiv.addEventListener("mouseover", function (e) {
+containerDiv.addEventListener("mouseenter", function (e) {
     if (e.target && e.target.matches(".cell")) {
         const enteredCell = e.target;
         enteredCell.setAttribute("style", "background: black;");
